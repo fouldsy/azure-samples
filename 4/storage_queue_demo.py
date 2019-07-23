21a759f149ffc2e00e386a23e2c663d96df7e38f
Terry Bates <tbates@linkedin.com>
2019-07-23 14:15:04 -0700

    Convert to Python3, slight changes to subproccess output processing, add python3 env shebang

--- 4/storage_queue_demo.py
+++ 4/storage_queue_demo.py
@@ -12,8 +12,8 @@ import string,random,time,azurerm,json,subprocess
 from azure.storage.queue import QueueService
 
 # Define variables to handle Azure authentication
-get_token_cli = subprocess.Popen(['az account get-access-token | jq  -r .accessToken'], stdout=subprocess.PIPE, shell=True)
-auth_token = str(get_token_cli.communicate()[0]).rstrip()
+get_token = subprocess.run(['az account get-access-token | jq  -r .accessToken'], stdout=subprocess.PIPE, shell=True)
+auth_token = get_token.stdout.decode('utf8').rstrip()
 subscription_id = azurerm.get_subscription_from_cli()
 
 # Define variables with random resource group and storage account names
@@ -27,14 +27,14 @@ location = 'eastus'
 ###
 response = azurerm.create_resource_group(auth_token, subscription_id, resourcegroup_name, location)
 if response.status_code == 200 or response.status_code == 201:
-    print('Resource group: ' + resourcegroup_name + ' created successfully.')
+    print(('Resource group: ' + resourcegroup_name + ' created successfully.'))
 else:
     print('Error creating resource group')
 
 # Create a storage account for our demo
 response = azurerm.create_storage_account(auth_token, subscription_id, resourcegroup_name, storageaccount_name,  location, storage_type='Standard_LRS')
 if response.status_code == 202:
-    print('Storage account: ' + storageaccount_name + ' created successfully.')
+    print(('Storage account: ' + storageaccount_name + ' created successfully.'))
     print('\nWaiting for storage account to be ready before we create a Queue')
     time.sleep(15)
 else:
@@ -45,7 +45,7 @@ else:
 # Use the Azure Storage Storage SDK for Python to create a Queue
 ###
 print('\nLet\'s create an Azure Storage Queue to drop some messages on.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 # Each storage account has a primary and secondary access key.
 # These keys are used by aplications to access data in your storage account, such as Queues.
@@ -68,14 +68,14 @@ else:
 # Use the Azure Storage Storage SDK for Python to drop some messages in our Queue
 ###
 print('Now let\'s drop some messages in our Queue.\nThese messages could indicate a take-out order being received for a customer ordering pizza.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 # This basic example creates a message for each pizza ordered. The message is *put* on the Queue.
-queue_service.put_message('pizzaqueue', u'Veggie pizza ordered.')
-queue_service.put_message('pizzaqueue', u'Pepperoni pizza ordered.')
-queue_service.put_message('pizzaqueue', u'Hawiian pizza ordered.')
-queue_service.put_message('pizzaqueue', u'Pepperoni pizza ordered.')
-queue_service.put_message('pizzaqueue', u'Pepperoni pizza ordered.')
+queue_service.put_message('pizzaqueue', 'Veggie pizza ordered.')
+queue_service.put_message('pizzaqueue', 'Pepperoni pizza ordered.')
+queue_service.put_message('pizzaqueue', 'Hawiian pizza ordered.')
+queue_service.put_message('pizzaqueue', 'Pepperoni pizza ordered.')
+queue_service.put_message('pizzaqueue', 'Pepperoni pizza ordered.')
 
 
 time.sleep(1)
@@ -85,10 +85,10 @@ time.sleep(1)
 # Use the Azure Storage Storage SDK for Python to count how many messages are in the Queue
 ###
 print('\nLet\'s see how many orders we have to start cooking! Here, we simply examine how many messages are sitting the Queue. ')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 metadata = queue_service.get_queue_metadata('pizzaqueue')
-print('Number of messages in the queue: ' + str(metadata.approximate_message_count))
+print(('Number of messages in the queue: ' + str(metadata.approximate_message_count)))
 
 
 time.sleep(1)
@@ -98,22 +98,22 @@ time.sleep(1)
 # Use the Azure Storage Storage SDK for Python to read each message from the Queue
 ###
 print('\nWith some messages in our Azure Storage Queue, let\'s read the first message in the Queue to signal we start to process that customer\'s order.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 # When you get each message, they become hidden from other parts of the applications being able to see it.
 # Once you have successfully processed the message, you then delete the message from the Queue.
 # This behavior makes sure that if something goes wrong in the processing of the message, it is then dropped back in the Queue for processing in the next cycle.
 messages = queue_service.get_messages('pizzaqueue')
 for message in messages:
-    print('\n' + message.content)
+    print(('\n' + message.content))
     queue_service.delete_message('pizzaqueue', message.id, message.pop_receipt)
 
-raw_input('\nPress Enter to continue...')
+input('\nPress Enter to continue...')
 metadata = queue_service.get_queue_metadata('pizzaqueue')
 
 print('If we look at the Queue again, we have one less message to show we have processed that order and a yummy pizza will be on it\'s way to the customer soon.')
-print('Number of messages in the queue: ' + str(metadata.approximate_message_count))
-raw_input('\nPress Enter to continue...')
+print(('Number of messages in the queue: ' + str(metadata.approximate_message_count)))
+input('\nPress Enter to continue...')
 
 
 ###
@@ -121,7 +121,7 @@ raw_input('\nPress Enter to continue...')
 # Although the actual cost is minimal since we deleted all the messages from the Queue, it's good to clean up resources when you're done
 ###
 print('\nThis is a basic example of how Azure Storage Queues behave.\nTo keep things tidy, let\'s clean up the Azure Storage resources we created.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 response = queue_service.delete_queue('pizzaqueue')
 if response == True:
@@ -131,6 +131,6 @@ else:
 
 response = azurerm.delete_resource_group(auth_token, subscription_id, resourcegroup_name)
 if response.status_code == 202:
-    print('Resource group: ' + resourcegroup_name + ' deleted successfully.')
+    print(('Resource group: ' + resourcegroup_name + ' deleted successfully.'))
 else:
     print('Error deleting resource group.')
--- 4/storage_table_demo.py
+++ 4/storage_table_demo.py
@@ -1,4 +1,4 @@
-# This script sample is part of "Learn Azure in a Month of Lunches" (Manning
+.# This script sample is part of "Learn Azure in a Month of Lunches" (Manning
 # Publications) by Iain Foulds.
 #
 # This sample script covers the exercises from chapter 4 of the book. For more
@@ -14,14 +14,14 @@ from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
 
 # Define variables to handle Azure authentication
-get_token_cli = subprocess.Popen(['az account get-access-token | jq  -r .accessToken'], stdout=subprocess.PIPE, shell=True)
-auth_token = str(get_token_cli.communicate()[0]).rstrip()
+get_token = subprocess.run(['az account get-access-token | jq  -r .accessToken'], stdout=subprocess.PIPE, shell=True)
+auth_token = get_token.stdout.decode('utf8').rstrip()
 subscription_id = azurerm.get_subscription_from_cli()
 
 # Define variables with random resource group and storage account names
 resourcegroup_name = 'azuremol'+''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))
 storageaccount_name = 'azuremol'+''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))
-location = 'eastus'
+location = 'westus'
 
 ###
 # Create the a resource group for our demo
@@ -29,14 +29,14 @@ location = 'eastus'
 ###
 response = azurerm.create_resource_group(auth_token, subscription_id, resourcegroup_name, location)
 if response.status_code == 200 or response.status_code == 201:
-    print('Resource group: ' + resourcegroup_name + ' created successfully.')
+    print(('Resource group: ' + resourcegroup_name + ' created successfully.'))
 else:
     print('Error creating resource group')
 
 # Create a storage account for our demo
 response = azurerm.create_storage_account(auth_token, subscription_id, resourcegroup_name, storageaccount_name,  location, storage_type='Standard_LRS')
 if response.status_code == 202:
-    print('Storage account: ' + storageaccount_name + ' created successfully.')
+    print(('Storage account: ' + storageaccount_name + ' created successfully.'))
     print('\nWaiting for storage account to be ready before we create a Table')
     time.sleep(15)
 else:
@@ -47,7 +47,7 @@ else:
 # Use the Azure Storage Storage SDK for Python to create a Table
 ###
 print('\nLet\'s create an Azure Storage Table to store some data.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 # Each storage account has a primary and secondary access key.
 # These keys are used by aplications to access data in your storage account, such as Tables.
@@ -72,9 +72,9 @@ time.sleep(1)
 # Use the Azure Storage Storage SDK for Python to create some entries in the Table
 ###
 print('Now let\'s add some entries to our Table.\nRemember, Azure Storage Tables is a NoSQL datastore, so this is similar to adding records to a database.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
-# Each entry in a Table is called an 'entity'. 
+# Each entry in a Table is called an 'entity'.
 # Here, we add an entry for first pizza with two pieces of data - the name, and the cost
 #
 # A partition key tracks how like-minded entries in the Table are created and queried.
@@ -112,14 +112,14 @@ time.sleep(1)
 # Use the Azure Storage Storage SDK for Python to query for entities in our Table
 ###
 print('With some data in our Azure Storage Table, we can query the data.\nLet\'s see what the pizza menu looks like.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 # In this query, you define the partition key to search within, and then which properties to retrieve
 # Structuring queries like this improves performance as your application scales up and keeps the queries efficient
 pizzas = table_service.query_entities('pizzatable', filter="PartitionKey eq 'pizzamenu'", select='description,cost')
 for pizza in pizzas:
-    print('Name: ' + pizza.description)
-    print('Cost: ' + str(pizza.cost) + '\n')
+    print(('Name: ' + pizza.description))
+    print(('Cost: ' + str(pizza.cost) + '\n'))
 
 time.sleep(1)
 
@@ -129,7 +129,7 @@ time.sleep(1)
 # Although the actual cost is minimal (fractions of a cent per month) for the three entities we created, it's good to clean up resources when you're done
 ###
 print('\nThis is a basic example of how Azure Storage Tables behave like a database.\nTo keep things tidy, let\'s clean up the Azure Storage resources we created.')
-raw_input('Press Enter to continue...')
+input('Press Enter to continue...')
 
 response = table_service.delete_table('pizzatable')
 if response == True:
@@ -139,6 +139,6 @@ else:
 
 response = azurerm.delete_resource_group(auth_token, subscription_id, resourcegroup_name)
 if response.status_code == 202:
-    print('Resource group: ' + resourcegroup_name + ' deleted successfully.')
+    print(('Resource group: ' + resourcegroup_name + ' deleted successfully.'))
 else:
     print('Error deleting resource group.')
